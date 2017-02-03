e23b52917249aad0c4a506b0679ee67397f6fe97
Jesus Imery <yisas_0929@hotmail.com>
2017-02-03 16:40:35 -0500

    #3 timekeeper test script

---
+++ Assets/Scripts/Test Scripts/TimekeeperTest.cs
@@ -0,0 +1,20 @@
+﻿using Chronos;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimekeeperTest : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetButtonDown("Test Button"))
+        {
+            GetComponent<GlobalClock>().localTimeScale = 0.5f;
+        }
+	}
+}
