a8321444f6df71c00e8d892054bfb6f78ed80571
Vladimir de la Cruz <vladimirdlc@gmail.com>
2017-02-05 23:34:04 -0500

    Translating cameras between sections

---
+++ Assets/Scripts/Gameplay/Camera/Overseer/OverseerCamera.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OverseerCamera : MonoBehaviour {
+    public OverseerTarget target;
+    public string axisKey;
+    public float delayTime = 3;
+    public float currentDelayTime = 0;
+    private float invDegree = 0.5f;
+
+	// Update is called once per frame
+	void Update () {
+        if (currentDelayTime < 0)
+        {
+            Debug.Log(transform.rotation.y);
+            if (Input.GetAxis(axisKey) > 0 && transform.rotation.y > invDegree || Input.GetAxis(axisKey) < 0 && transform.rotation.y < invDegree)
+            {
+                if (target.nextTarget)
+                {
+                    GetComponent<RTSCamera>().followTarget = target.nextTarget.transform;
+                    target = target.nextTarget;
+                    startCooldown();
+                }
+            }
+            if (Input.GetAxis(axisKey) < 0 && transform.rotation.y > invDegree || Input.GetAxis(axisKey) > 0 && transform.rotation.y < invDegree)
+            {
+                if (target.previousTarget)
+                {
+                    GetComponent<RTSCamera>().followTarget = target.previousTarget.transform;
+                    target = target.previousTarget;
+                    startCooldown();
+                }
+            }
+        }
+
+        currentDelayTime -= Time.deltaTime;
+    }
+
+    void startCooldown()
+    {
+        currentDelayTime = delayTime;
+    }
+}
---
+++ Assets/Scripts/Gameplay/Camera/Overseer/OverseerTarget.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OverseerTarget : MonoBehaviour {
+    public OverseerTarget nextTarget;
+    public OverseerTarget previousTarget;
+}
