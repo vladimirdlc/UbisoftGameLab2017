5e57c98941729315c82a4385116e2434c5237211
Vladimir de la Cruz <vladimirdlc@gmail.com>
2017-02-09 23:09:48 -0600

    #2 Added Input Control detection for Overseer
    Added Red, Blue, Green materials

---
+++ Assets/Scripts/OverSeer/OverseerControls.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OverseerControls : MonoBehaviour {
+    public string actionButtonX;
+    public string actionButtonA;
+    public string actionButtonB;
+    public string actionButtonY;
+
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetButtonDown(actionButtonA))
+        {
+            Debug.Log("A");
+        }
+        if (Input.GetButtonDown(actionButtonX))
+        {
+            Debug.Log("X");
+        }
+        if (Input.GetButtonDown(actionButtonB))
+        {
+            Debug.Log("B");
+        }
+        if (Input.GetButtonDown(actionButtonY))
+        {
+            Debug.Log("Y");
+        }
+    }
+}
