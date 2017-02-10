dd6c9566a9e3c26e38a83b5d54380d50f68a02fd
Vladimir de la Cruz <vladimirdlc@gmail.com>
2017-02-10 01:49:49 -0600

    #2 Control Gates by Overseer
    Currently using A and X from Controller

---
+++ Assets/Scripts/Controls/OverSeer/Controllables/OSAnimatorControl.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OSAnimatorControl : MonoBehaviour, OSControllable {
+
+    void OSControllable.triggerAction()
+    {
+        Debug.Log("A");
+        GetComponent<Animator>().SetTrigger("toggleObject");
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/Controls/OverSeer/OverseerControls.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OverseerControls : MonoBehaviour {
+    public string actionButtonX;
+    public string actionButtonA;
+    public string actionButtonB;
+    public string actionButtonY;
+
+    public List<GameObject> controllablesX;
+    public List<GameObject> controllablesA;
+    public List<GameObject> controllablesB;
+    public List<GameObject> controllablesY;
+
+    // Update is called once per frame
+    void Update () {
+        if (Input.GetButtonDown(actionButtonA))
+        {
+            foreach(GameObject control in controllablesA)
+            {
+                OSControllable trigger = control.GetComponent(typeof(OSControllable)) as OSControllable;
+                trigger.triggerAction();
+            }
+        }
+        if (Input.GetButtonDown(actionButtonX))
+        {
+            foreach (GameObject control in controllablesX)
+            {
+                OSControllable trigger = control.GetComponent(typeof(OSControllable)) as OSControllable;
+                trigger.triggerAction();
+            }
+        }
+        if (Input.GetButtonDown(actionButtonB))
+        {
+            foreach (GameObject control in controllablesB)
+            {
+                OSControllable trigger = control.GetComponent(typeof(OSControllable)) as OSControllable;
+                trigger.triggerAction();
+            }
+        }
+        if (Input.GetButtonDown(actionButtonY))
+        {
+            foreach (GameObject control in controllablesY)
+            {
+                OSControllable trigger = control.GetComponent(typeof(OSControllable)) as OSControllable;
+                trigger.triggerAction();
+            }
+        }
+    }
+}
