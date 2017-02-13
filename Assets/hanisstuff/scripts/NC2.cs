d3c2ec0b005003d00bd354f8a14e1774b43d5864
hani-sayegh <papaaced@gmail.com>
2017-02-12 22:27:44 -0500

    #5 some fix

--- Assets/hanisstuff/scripts/NC2.cs
+++ Assets/hanisstuff/scripts/NC2.cs
@@ -16,9 +16,14 @@ public class NC2 : NetworkManager
     public override void OnClientConnect(NetworkConnection conn)
     {
         NetworkMessage test = new NetworkMessage();
-        test.chosenClass = 0;
-        print("Called");
-        ClientScene.AddPlayer(conn, 1, test);
+        test.chosenClass = 1;
+        Debug.Log("Called");
+        ClientScene.Ready(conn);
+        if (test.chosenClass == 0)
+            ClientScene.RegisterPrefab(ground);
+        else
+            ClientScene.RegisterPrefab(god);
+        ClientScene.AddPlayer(conn, 0, test);
     }
 
     public override void OnServerAddPlayer(NetworkConnection conn, short playerControllerId, NetworkReader extraMessageReader)
@@ -26,8 +31,17 @@ public class NC2 : NetworkManager
         print("DSDS");
         NetworkMessage message = extraMessageReader.ReadMessage<NetworkMessage>();
         int mes = message.chosenClass;
-        GameObject player = Instantiate(mes == 0 ? ground : god) as GameObject;
-
-        NetworkServer.AddPlayerForConnection(conn, player, playerControllerId);
+        //if (mes == 0)
+        //{
+        //    base.OnServerAddPlayer(conn, playerControllerId);
+        //}
+        //else
+        if (mes == 1)
+            NetworkServer.Spawn(god);
+        else
+        {
+            GameObject player = Instantiate(mes == 0 ? ground : god) as GameObject;
+            NetworkServer.AddPlayerForConnection(conn, player, playerControllerId);
+        }
     }
 }
