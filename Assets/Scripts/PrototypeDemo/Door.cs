faa533877ad3e7867d6877f0f7417aa812b05980
Pierre-André Léger <pierrealeger83@gmail.com>
2017-02-15 05:53:17 -0500

    #4 Friday's Prototype Level
    Made some quick and dirty doors and pressure plates, doors implement
    OSControllable. Just slapped on the default character controller for the
    dog. Haven't implemented the hiding spot, not sure yet how the proximity
    paradox is implemented so I just laid down a slab of concrete so the
    "dog" can get out of range

---
+++ Assets/Scripts/PrototypeDemo/Door.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour, OSControllable {
+
+    public GameObject mesh;
+    public bool isTimed;
+    public GameObject[] pressurePlates;
+    public float timer;
+    public float coolDown;
+    public bool openByDefault;
+
+
+    private int count;
+    private float closeAtTime;
+    private float readyAtTime;
+    private bool isOpen;
+
+    private void Start()
+    {
+        count = 0;
+        isOpen = false;
+        closeAtTime = 0;
+        if (openByDefault) Open();
+    }
+
+    private void Update()
+    {
+        if (isOpen && isTimed)
+        {
+            if (Time.time >= closeAtTime) Close();
+        }
+    }
+
+    public void IncCount() {
+        count++;
+        if (count == pressurePlates.Length)
+            Open();
+    }
+
+    public void DecCount() {
+        count--;
+    }
+
+    public void Open() {
+        isOpen = true;
+        readyAtTime = Time.time + coolDown;
+        closeAtTime = Time.time + timer;
+        mesh.GetComponent<Transform>().Translate(new Vector3(0, 3, 0));
+    }
+
+    public void Close() {
+        isOpen = false;
+        readyAtTime = Time.time + coolDown;
+        mesh.GetComponent<Transform>().Translate(new Vector3(0, -3, 0));
+    }
+
+    public void triggerAction()
+    {
+        if (!isOpen && Time.time >= readyAtTime)
+        {
+            Open();
+        }
+        else if (isOpen && Time.time >= readyAtTime)
+        {
+            Close();
+        }
+    }
+}
---
+++ Assets/Scripts/PrototypeDemo/PressurePlate.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PressurePlate : MonoBehaviour
+{
+    public GameObject mesh;
+    public Material active;
+    public Material inactive;
+
+    public GameObject target;
+
+    void Start()
+    {
+        mesh.GetComponent<Renderer>().material = inactive;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        target.GetComponent<Door>().IncCount();
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        target.GetComponent<Door>().DecCount();
+    }
+}
