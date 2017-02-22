eb5f0b128fc01c735c8a9a105b14b8d93a058b82
Jesus Imery <yisas_0929@hotmail.com>
2017-02-22 00:40:19 -0500

    #1 just a quick test of the dog animation

---
+++ Assets/Scripts/Test Scripts/TestDog.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestDog : MonoBehaviour {
+
+    Animator anim;
+    Rigidbody rb;
+
+    public float walkingSpeed;
+    public float animatorWalkingSpeed;
+
+	// Use this for initialization
+	void Start () {
+        anim = GetComponentInChildren<Animator>();
+        rb = GetComponent<Rigidbody>();
+
+        anim.SetFloat("walkingSpeed", animatorWalkingSpeed);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float horizontalInput = Input.GetAxis("Horizontal Ground");
+        float verticalInput = Input.GetAxis("Vertical Ground");
+
+        rb.AddForce(new Vector3(horizontalInput, 0, verticalInput) * walkingSpeed);
+	}
+}
