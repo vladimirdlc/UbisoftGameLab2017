4df75487dac3c2a3aaf02fd3b05ba8522bda43ad
Jesus Imery <yisas_0929@hotmail.com>
2017-02-24 23:43:36 -0500

    #3 Time paradoxes work! Dumb bug was dumb! Rewinding throwing indexing erros for the paradoxed shadows

--- Assets/Scripts/Utility/CustomCollectionManipulation.cs
+++ Assets/Scripts/Utility/CustomCollectionManipulation.cs
@@ -6,10 +6,15 @@ public class CustomCollectionManipulation : MonoBehaviour {
 	
     public static List<T> RemoveElementsAfterIndex<T>(List<T> list, int index)
     {
+
+        list.RemoveRange(index,list.Count - index);
+
+        /* This is wrong and I am dumb
         for (int i = index; i < list.Count; i++)
         {
-            list.RemoveAt(i);
+            list.RemoveAt(index);
         }
+        /*/
 
         return list;
     }
