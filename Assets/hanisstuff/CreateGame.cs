1cdc3bf16d57a17d6f76473e0b70af7b2ee83cc0
hani-sayegh <papaaced@gmail.com>
2017-02-26 16:07:15 -0500

    #5 being networking ui

---
+++ Assets/hanisstuff/CreateGame.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+public class CreateGame : MonoBehaviour
+{
+    private NetworkManager networkManager;
+    void Start()
+    {
+        networkManager = NetworkManager.singleton;
+        //networkManager.
+    }
+}
