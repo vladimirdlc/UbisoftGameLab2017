a02d331d1f5afe2a44514131a8f2bf1779ee787a
hani-sayegh <papaaced@gmail.com>
2017-02-26 21:50:44 -0500

    implemented local discovery for joining game 'randomly'

--- Assets/hanisstuff/CreateGame.cs
+++ Assets/hanisstuff/CreateGame.cs
@@ -1,12 +1,52 @@
 ﻿using UnityEngine;
 using UnityEngine.Networking;
 
-public class CreateGame : MonoBehaviour
+public class CreateGame : NetworkDiscovery
 {
     private NetworkManager networkManager;
+    //public NetworkDiscovery networkDiscovery;
+    private float timeOut = 2f;
     void Start()
     {
+        Initialize();
         networkManager = NetworkManager.singleton;
-        //networkManager.
+        //why this works?????? new does not work if part of monobeha??
+        //networkDiscovery = this;
+        //networkDiscovery.Initialize();
+        //networkDiscovery.StartAsClient();
+        //networkDiscovery.StartAsServer();
+        StartAsClient();
+    }
+
+    bool noMoreUpdate = false;
+    //YOU NEED LATEUPDATE HERE, OFCOURSE UNITY DOCUMENTATION DOESNT SAY THAT:
+    //http://answers.unity3d.com/questions/1214729/onreceivedbroadcast-not-being-called-with-network.html
+    void LateUpdate()
+    {
+        if (noMoreUpdate)
+            return;
+        timeOut -= Time.deltaTime;
+        if (timeOut < 0)
+        {
+            //networkDiscovery.StopBroadcast();
+            StopBroadcast();
+            networkManager.StartHost();
+            //networkDiscovery.Initialize();
+            Initialize();
+            //networkDiscovery.broadcastData = "WORKKKK";
+            //networkDiscovery.StartAsServer();
+            StartAsServer();
+            noMoreUpdate = true;
+        }
+    }
+    public override void OnReceivedBroadcast(string fromAddress, string data)
+    {
+        base.OnReceivedBroadcast(fromAddress, data);
+        Debug.Log("sfdesdf");
+        //if (noMoreUpdate)
+        //    return;
+        networkManager.networkAddress = fromAddress;
+        networkManager.StartClient();
+        noMoreUpdate = true;
     }
 }
