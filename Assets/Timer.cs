ceda22493745a63df26220bb07ce4ba0fc4ec38c
hani-sayegh <papaaced@gmail.com>
2017-03-01 22:04:36 -0500

    Added timer

---
+++ Assets/Timer.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine.UI;
+using UnityEngine;
+
+public class Timer : MonoBehaviour
+{
+    public Text timer;
+    private float spawnTime;
+
+    // Use this for initialization
+    void Start()
+    {
+        spawnTime = Time.time;
+        timer = GameObject.Find("Timer").GetComponent<Text>();
+    }
+
+    void Update()
+    {
+        timer.text = Mathf.Floor(Time.time - spawnTime).ToString();
+    }
+}
