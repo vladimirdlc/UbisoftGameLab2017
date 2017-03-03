96948460fe20d5d028b34bd50e3bd6f3f8377430
hani-sayegh <papaaced@gmail.com>
2017-03-03 14:41:56 -0600

    cerating main menus

---
+++ Assets/HostGame.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+public class HostGame : MonoBehaviour
+{
+
+    private NetworkManager netManager;
+
+    void Start()
+    {
+        netManager = NetworkManager.singleton;
+        gameObject.GetComponent<NetworkManagerHUD>().showGUI = false;
+    }
+
+    public void PlayGameHost()
+    {
+        netManager.StartHost();
+    }
+}
