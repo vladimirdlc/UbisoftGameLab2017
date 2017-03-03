b7d802f42ddb8089dd9ef5691e46c18fc57c3d18
Jesus Imery <yisas_0929@hotmail.com>
2017-03-02 20:12:57 -0500

    #4 WIP new level 1 setup

--- Assets/PATimeRefactor/Assets/Scripts/PlayerUserController.cs
+++ Assets/PATimeRefactor/Assets/Scripts/PlayerUserController.cs
@@ -6,7 +6,7 @@ using UnityStandardAssets.CrossPlatformInput;
 public class PlayerUserController : MonoBehaviour
 {
     private Character m_Character;          // A reference to the ThirdPersonCharacter on the object
-    private Transform m_Cam;                  // A reference to the main camera in the scenes transform
+    public Transform m_Cam;                  // A reference to the main camera in the scenes transform
     private Vector3 m_CamForward;             // The current forward direction of the camera
     private Vector3 m_Move;
 
@@ -33,28 +33,29 @@ public class PlayerUserController : MonoBehaviour
             m_Move = 0 * Vector3.forward + 0 * Vector3.right;
         }
 
-        // This is the original script for Ethan's movement. The actual controller should use something relative
-        // to the attached camera on the player (like what they are doing here).
-        // The current version uses world space to figure out where to go.
+        //This is the original script for Ethan's movement. The actual controller should use something relative
 
-        //            // calculate move direction to pass to character
-        //            if (m_Cam != null)
-        //            {
-        //                // calculate camera relative direction to move:
-        //                m_CamForward = Vector3.Scale(m_Cam.forward, new Vector3(1, 0, 1)).normalized;
-        //                m_Move = v*m_CamForward + h*m_Cam.right;
-        //            }
-        //            else
-        //            {
-        //                // we use world-relative directions in the case of no main camera
-        //                m_Move = v*Vector3.forward + h*Vector3.right;
-        //            }
-        //#if !MOBILE_INPUT
-        //			// walk speed multiplier
-        //	        if (Input.GetKey(KeyCode.LeftShift)) m_Move *= 0.5f;
-        //#endif
+        //to the attached camera on the player(like what they are doing here).
+        //The current version uses world space to figure out where to go.
 
-        // pass all parameters to the character control script
+                    // calculate move direction to pass to character
+            if (m_Cam != null)
+            {
+                // calculate camera relative direction to move:
+                m_CamForward = Vector3.Scale(m_Cam.forward, new Vector3(1, 0, 1)).normalized;
+                m_Move = v * m_CamForward + h * m_Cam.right;
+            }
+            else
+            {
+                // we use world-relative directions in the case of no main camera
+                m_Move = v * Vector3.forward + h * Vector3.right;
+            }
+#if !MOBILE_INPUT
+        // walk speed multiplier
+        if (Input.GetKey(KeyCode.LeftShift)) m_Move *= 0.5f;
+#endif
+
+        //pass all parameters to the character control script
 
         m_Character.Move(m_Move, crouch);
     }
