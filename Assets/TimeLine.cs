856b7a6ec70bef555bfd6ee86663222c3575dada
hani-sayegh <papaaced@gmail.com>
2017-03-07 22:44:14 -0500

    dynamically create seconds

--- Assets/TimeLine.cs
+++ Assets/TimeLine.cs
@@ -1,19 +1,28 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class TimeLine : MonoBehaviour
 {
-
+    //hide the usual transform since it doesn't make since for UI
     new RectTransform transform;
+
+    public float widthBetweenSeconds;
+    public int intervalCount = 10;
+    public Text second;
     void Start()
     {
+        var timeLine = GameObject.FindGameObjectWithTag("TimeLine");
         transform = GetComponent<RectTransform>();
-
+        widthBetweenSeconds = Screen.width / 10;
+        for (int i = 0; i < intervalCount; ++i)
+        {
+            var temp = Instantiate(second);
+            temp.transform.parent = timeLine.transform;
+        }
     }
-    void Update()
+    void LateUpdate()
     {
-        transform.anchoredPosition += Vector2.right * Time.deltaTime * 10;
+        transform.anchoredPosition += Vector2.right * Time.deltaTime * 100;
     }
 }
