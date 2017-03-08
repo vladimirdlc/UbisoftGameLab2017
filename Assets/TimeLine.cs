ea5cb43b45a243582b58ba37523bd1f343b2bc7c
hani-sayegh <papaaced@gmail.com>
2017-03-08 13:47:06 -0600

    Timeline now showing infinite number of seconds

--- Assets/TimeLine.cs
+++ Assets/TimeLine.cs
@@ -11,9 +11,15 @@ public class TimeLine : MonoBehaviour
     //10 intervals should mean 11 positions, starting at 0s
     public int intervalCount = 10;
     public Text second;
+    private float midpoint;
+    private List<Text> secondsOnScreen;
+    private int latestSecond;
 
     void Start()
     {
+        latestSecond = intervalCount;
+        midpoint = Screen.width / 2;
+        secondsOnScreen = new List<Text>();
         var timeLine = GameObject.FindGameObjectWithTag("TimeLine");
         transform = GetComponent<RectTransform>();
         widthBetweenSeconds = Screen.width / (float)10;
@@ -23,10 +29,29 @@ public class TimeLine : MonoBehaviour
             temp.transform.parent = timeLine.transform;
             temp.text = i.ToString();
             temp.rectTransform.anchoredPosition = new Vector2(i * widthBetweenSeconds, 0);
+            secondsOnScreen.Add(temp);
         }
     }
-    void LateUpdate()
+    void Update()
     {
-        transform.anchoredPosition += Vector2.right * Time.deltaTime * 100;
+        if (transform.anchoredPosition.x < midpoint)
+            transform.anchoredPosition += Vector2.right * Time.deltaTime * 500;
+        else
+        {
+            foreach (var second in secondsOnScreen)
+            {
+                second.rectTransform.anchoredPosition += Vector2.left * 0.5f;
+                var charOrigin = second.rectTransform.anchoredPosition.x;
+                //In pixels
+                var charWidth = second.font.characterInfo[0].advance;
+
+                if (charOrigin + charWidth + widthBetweenSeconds < 0)
+                {
+                    second.rectTransform.anchoredPosition = Vector2.right * Screen.width;
+                    second.text = (latestSecond++ + 1).ToString();
+                }
+            }
+        }
+
     }
 }
