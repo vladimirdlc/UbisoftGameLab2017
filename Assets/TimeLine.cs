3f05e2b5be0cdb23ac420ac596cfcc52b01c9296
hani-sayegh <papaaced@gmail.com>
2017-03-07 22:57:10 -0500

    display seconds in correct positions

--- Assets/TimeLine.cs
+++ Assets/TimeLine.cs
@@ -8,17 +8,21 @@ public class TimeLine : MonoBehaviour
     new RectTransform transform;
 
     public float widthBetweenSeconds;
+    //10 intervals should mean 11 positions, starting at 0s
     public int intervalCount = 10;
     public Text second;
+
     void Start()
     {
         var timeLine = GameObject.FindGameObjectWithTag("TimeLine");
         transform = GetComponent<RectTransform>();
-        widthBetweenSeconds = Screen.width / 10;
-        for (int i = 0; i < intervalCount; ++i)
+        widthBetweenSeconds = Screen.width / (float)10;
+        for (int i = 0; i < intervalCount + 1; ++i)
         {
             var temp = Instantiate(second);
             temp.transform.parent = timeLine.transform;
+            temp.text = i.ToString();
+            temp.rectTransform.anchoredPosition = new Vector2(i * widthBetweenSeconds, 0);
         }
     }
     void LateUpdate()
