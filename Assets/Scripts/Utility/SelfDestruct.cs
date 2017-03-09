6303a74a550a5bc88a24f00f28dfe912edf0f7db
Jesus Imery <yisas_0929@hotmail.com>
2017-03-09 01:10:51 -0500

    #3 Placeholder warp effects tweaks

---
+++ Assets/Scripts/Utility/SelfDestruct.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelfDestruct : MonoBehaviour {
+
+    public float selfDesctructCountDown;
+    public bool onlyDeactivate;
+
+    private float timer;
+
+	// Use this for initialization
+	void Start () {
+        timer = selfDesctructCountDown;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        timer -= Time.deltaTime;
+
+        if (timer <= 0)
+            if (onlyDeactivate)
+                gameObject.SetActive(false);
+            else
+                Destroy(gameObject);
+	}
+}
