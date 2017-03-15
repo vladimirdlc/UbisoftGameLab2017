6ce61b578c7aa06309553983d2aeb525289abf1c
hani-sayegh <papaaced@gmail.com>
2017-03-15 12:24:48 -0400

    fix bug not let you build

--- Assets/Scripts/UI/Tutorials/TutorialTriggerCustomInspector.cs
+++ Assets/Scripts/UI/Tutorials/TutorialTriggerCustomInspector.cs
@@ -1,4 +1,5 @@
-﻿using UnityEditor;
+﻿#if UNITY_EDITOR
+using UnityEditor;
 using UnityEngine;
 
 [CustomEditor(typeof(TutorialTrigger))]
@@ -21,3 +22,4 @@ public class TutorialTriggerCustomInspector : Editor
         EditorUtility.SetDirty(target);
     }
 }
+#endif
