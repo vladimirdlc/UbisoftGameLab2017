0df235a118099a70b5cb06b9760d56f93353a301
Jesus Imery <yisas_0929@hotmail.com>
2017-03-16 00:16:13 -0400

    #1 Dog sits when time is stopped

--- Assets/Scripts/Dog/AnimatedDog.cs
+++ Assets/Scripts/Dog/AnimatedDog.cs
@@ -7,7 +7,9 @@ public class AnimatedDog : MonoBehaviour {
     protected Animator m_Animator;
     protected Rigidbody m_RigidBody;
 
+    [Header("---- Animator Variables ----")]
     public float walkAnimSpeed;
+    public float sitAnimSpeed;
 
     public virtual void Start()
     {
@@ -15,9 +17,12 @@ public class AnimatedDog : MonoBehaviour {
         m_Animator = GetComponentInChildren<Animator>();
     }
 
-    protected void UpdateAnimator()
+    protected void UpdateAnimator(bool sitting = false)
     {
         m_Animator.SetFloat("walkingSpeedMultiplier", walkAnimSpeed);
+        m_Animator.SetFloat("sitSpeed", sitAnimSpeed);
+        m_Animator.SetBool("sitting", sitting);
+
         Vector3 tempVector = m_RigidBody.velocity;
         tempVector.y = 0;
         m_Animator.SetFloat("walkingSpeed", tempVector.magnitude);
