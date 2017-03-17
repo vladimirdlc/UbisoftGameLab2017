8d5392626ca176deb0b7ec28d3a970b785c63b89
Jesus Imery <yisas_0929@hotmail.com>
2017-03-17 05:19:44 -0400

    #3 "Fixed" warp bubble sounds on certain behaviours

---
+++ Assets/PATimeRefactor/Assets/Scripts/TimeManagerDependable.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// For general purpose behaviour that needs to know game states
+/// </summary>
+public class TimeManagerDependable : MonoBehaviour
+{
+
+    public enum RequiredAction { Destroy, Deactivate, TurnOffAudiosource, StopAudioSource }
+
+    public TimeManager.GameState[] m_TriggeringGameStates;
+    public RequiredAction m_RequiredAction;
+
+    // Necessary references
+    private TimeManager m_TimeManager;
+
+    // Optional references
+    private AudioSource m_AudioSource;
+
+    // Use this for initialization
+    void Start()
+    {
+        m_TimeManager = GameObject.FindGameObjectWithTag("Time Manager").GetComponent<TimeManager>();
+
+        if (m_RequiredAction == RequiredAction.TurnOffAudiosource)
+            m_AudioSource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        TimeManager.GameState frameGameState = m_TimeManager.m_GameState;
+
+        if (CheckForGameState(frameGameState))
+        {
+            switch (m_RequiredAction)
+            {
+                case RequiredAction.Destroy:
+                    Destroy(gameObject);
+                    break;
+
+                case RequiredAction.Deactivate:
+                    gameObject.SetActive(false);
+                    break;
+
+                case RequiredAction.TurnOffAudiosource:
+                    m_AudioSource.enabled = false;
+                    break;
+
+                case RequiredAction.StopAudioSource:
+                    m_AudioSource.Stop();
+                    break;
+            }
+        }
+    }
+
+    private bool CheckForGameState(TimeManager.GameState searchedGameState)
+    {
+        bool result = false;
+
+        foreach (TimeManager.GameState comparedGameState in m_TriggeringGameStates)
+        {
+            if (comparedGameState == searchedGameState)
+            {
+                result = true;
+                break;
+            }
+        }
+
+        return result;
+    }
+}
