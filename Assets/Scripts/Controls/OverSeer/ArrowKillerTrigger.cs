ed6602abeff652372b2bd933423943003624352c
Vladimir de la Cruz <vladimirdlc@gmail.com>
2017-03-17 20:12:57 -0400

    #2 New spawnable arrow model and limiting to one arrow per SpawnPoint Making invisible Overseer movement arrow to Ground Player #1 Removing "Ground Environment Hidden" from the rendering of the Dog controller

---
+++ Assets/Scripts/Controls/OverSeer/ArrowKillerTrigger.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArrowKillerTrigger : MonoBehaviour {
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "SpawnedArrow" && 
+            other.GetComponentInParent<DestroyAfterTime>().currentTimeAlive > GetComponentInParent<DestroyAfterTime>().currentTimeAlive)
+        {
+            Destroy(other.transform.parent.gameObject);
+        }
+    }
+}
