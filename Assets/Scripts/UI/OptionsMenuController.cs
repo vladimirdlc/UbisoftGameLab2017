bfaacd2e2894f1164b675fd99b58a176c7b62dcc
Jesus Imery <yisas_0929@hotmail.com>
2017-03-21 01:51:13 -0400

    #7 Main menu options controller and canvas

---
+++ Assets/Scripts/UI/OptionsMenuController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OptionsMenuController : MonoBehaviour {
+
+    public Slider sensitivityX;
+    public Slider sensitivityY;
+    public Toggle invertYAxis;
+
+    //private void Update()
+    //{
+    //    Debug.Log(PlayerPrefs.GetFloat("SensitivityX"));
+    //}
+
+    public void UpdateSensitivityX()
+    {
+        PlayerPrefs.SetFloat("SensitivityX", sensitivityX.value);
+    }
+
+    public void UpdateSensitivityY()
+    {
+        PlayerPrefs.SetFloat("SensitivityY", sensitivityX.value);
+    }
+
+    public void UpdateInveryYAxis()
+    {
+        PlayerPrefs.SetInt("InvertY", invertYAxis.isOn ? 1 : -1);
+    }
+}
