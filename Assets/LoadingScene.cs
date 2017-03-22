6eca3ebd283ac2dc8b83cfa0c2bf9cb2749aaa9e
Jesus Imery <yisas_0929@hotmail.com>
2017-03-22 18:48:52 -0400

    #4 #7 Loading scene is a go

--- Assets/LoadingScene.cs
+++ Assets/LoadingScene.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class LoadingScene : MonoBehaviour {
@@ -11,10 +12,35 @@ public class LoadingScene : MonoBehaviour {
     public Animator m_CanvasAnimator;
     public Image m_ImageToChange;
     public Sprite[] m_BillboardImages;
+    public float posterDisplayTime;
+
+    private float timer;
+    private bool fading = false;
 
 	// Use this for initialization
 	void Start () {
         m_CanvasAnimator.SetTrigger("fadeIn");
         m_ImageToChange.sprite = m_BillboardImages[m_ImageToLoad];
+        timer = posterDisplayTime;
 	}
+
+    private void Update()
+    {
+        timer -= Time.deltaTime;
+
+        if(timer <= 0 && !fading)
+        {
+            fading = true;
+            m_CanvasAnimator.SetTrigger("fadeOut");
+        }
+    }
+
+    public void LoadScene()
+    {
+#if NETWORKING
+            UnityEngine.Networking.NetworkManager.singleton.ServerChangeScene(m_SceneToLoad);
+#else
+        SceneManager.LoadScene(m_SceneToLoad);
+#endif
+    }
 }
