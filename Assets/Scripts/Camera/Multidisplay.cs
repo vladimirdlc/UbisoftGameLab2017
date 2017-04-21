88adfc9e0c23c419d2b8459ec4809219acfa5457
Jesus Imery <yisas_0929@hotmail.com>
2017-04-21 18:44:27 -0400

    Multidisplay, again

--- Assets/Scripts/Camera/Multidisplay.cs
+++ Assets/Scripts/Camera/Multidisplay.cs
@@ -4,19 +4,19 @@ using UnityEngine;
 
 public class Multidisplay : MonoBehaviour {
 
+    private static bool mCheckDone = false;
+
 	// Use this for initialization
 	void Start () {
-        Debug.Log("displays connected: " + Display.displays.Length);
-        // Display.displays[0] is the primary, default display and is always ON.
-        // Check if additional displays are available and activate each.
-        if (Display.displays.Length > 1)
-            Display.displays[1].Activate();
-        if (Display.displays.Length > 2)
-            Display.displays[2].Activate();
-    }
 
-    // Update is called once per frame
-    void Update () {
-		
-	}
+        if (!mCheckDone)
+        {
+            // Display.displays[0] is the primary, default display and is always ON.
+            // Check if additional displays are available and activate each.
+            if (Display.displays.Length > 1)
+                Display.displays[1].Activate();
+
+            mCheckDone = false;
+        }
+    }
 }
